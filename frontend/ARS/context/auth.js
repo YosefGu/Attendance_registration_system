261f2e414a6e7fd70bc4c3d948ca269835b36bf0
YosefGu <yos770yos@gmail.com>
2024-11-28 14:25:18 +0200

    updating user global context and auth context

--- frontend/ARS/context/auth.js
+++ frontend/ARS/context/auth.js
@@ -5,28 +5,28 @@ import { deleteToken } from "../utils/tokenHandling";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
+  const logout = async () => {
+    await deleteToken();
+    setIsAuthenticated(false);
+  };
 
-    const logout = async () => {
-        await deleteToken();
-        setIsAuthenticated(false)
-    }
+  useEffect(() => {
+    const checkToken = async () => {
+      const isValid = await verifyToken();
+      setIsAuthenticated(isValid);
+      setLoading(false);
+    };
+    checkToken();
+  }, []);
 
-
-    useEffect(() => {
-        const checkToken = async () => {
-            const isValid = await verifyToken();
-            setIsAuthenticated(isValid)
-            setLoading(false)
-        }
-        checkToken();
-    }, [])
-
-    return(
-        <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated, loading, logout}}>
-            {!loading && children }
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, loading, logout }}
+    >
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
