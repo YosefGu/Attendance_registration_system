7b682d019c7fd3537b32605d4b6b3819adaad88e
YosefGu <yos770yos@gmail.com>
2024-11-28 14:44:40 +0200

    Essential updates

--- frontend/ARS/App.js
+++ frontend/ARS/App.js
@@ -1,43 +1,116 @@
-import { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useContext } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { AuthProvider, AuthContext } from './context/auth';
-import { UserProvider } from './context/userContext';
+import { AuthProvider, AuthContext } from "./context/auth";
+import { UserProvider } from "./context/userContext";
 
-import { Register } from './pages/register';
-import { Login } from './pages/login';
-import { Home } from './pages/home';
-import { Attendance } from './pages/Home/attendance';
-import { Setting } from './pages/Home/setting';
-import { Profile } from './pages/Setting/profile';
-import { ManageStudents } from './pages/Setting/manageStudents';
-import { ManagedTeam } from './pages/Setting/managedTeam';
-import { AddStudent } from './pages/Student/addStudent';
-import { AddTeam } from './pages/Setting/addTeam';
-
+import { Register } from "./pages/register";
+import { Login } from "./pages/login";
+import { Attendance } from "./pages/Home/attendance";
+import { Profile } from "./pages/Setting/profile";
+import { ManageStudents } from "./pages/Setting/manageStudents";
+import { ManagedTeam } from "./pages/Setting/managedTeam";
+import { AddStudent } from "./pages/Student/addStudent";
+import { AddTeam } from "./pages/Setting/addTeam";
+import { CustomHeader } from "./utils/customHeader";
+import { SideBar } from "./utils/sideBar";
+import { SendMessages } from "./pages/Home/sendMessages";
+import { Main } from "./pages/main";
 
 const Stack = createNativeStackNavigator();
 
 const AuthenticatedNavigator = () => (
   <UserProvider>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Attendance" component={Attendance} />
-      <Stack.Screen name="Setting" component={Setting} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="ManageStudents" component={ManageStudents} />
-      <Stack.Screen name="ManagedTeam" component={ManagedTeam} />
-      <Stack.Screen name="AddStudent" component={AddStudent} />
-      <Stack.Screen name='AddTeam' component={AddTeam}/>
+    <Stack.Navigator
+      screenOptions={({ navigation }) => ({
+        header: ({ options }) => (
+          <CustomHeader
+            title={options.title}
+            navigation={navigation}
+            navigateTo={options.navigateTo}
+          />
+        ),
+        animation: "slide_from_right",
+      })}
+    >
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Attendance"
+        component={Attendance}
+        options={{ title: "Attendance" }}
+      />
+      <Stack.Screen
+        name="SendMessages"
+        component={SendMessages}
+        options={{ title: "Send Messages" }}
+      />
+
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{ title: "Profile" }}
+      />
+
+      <Stack.Screen
+        name="ManageStudents"
+        component={ManageStudents}
+        options={{ title: "Manage Students" }}
+      />
+
+      <Stack.Screen
+        name="ManagedTeam"
+        component={ManagedTeam}
+        options={{ title: "Managed Team" }}
+      />
+
+      <Stack.Screen
+        name="AddStudent"
+        component={AddStudent}
+        options={{ title: "Add Student", navigateTo: "ManageStudents" }}
+      />
+
+      <Stack.Screen
+        name="AddTeam"
+        component={AddTeam}
+        options={{ title: "Add Team", navigateTo: "ManagedTeam" }}
+      />
+      <Stack.Screen
+        name="SideBar"
+        component={SideBar}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   </UserProvider>
 );
 
 const UnauthenticatedNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="Register" component={Register} />
+  <Stack.Navigator
+    screenOptions={({ navigation }) => ({
+      header: ({ options }) => (
+        <CustomHeader
+          title={options.title}
+          navigation={navigation}
+          navigateTo={options.navigateTo}
+        />
+      ),
+      animation: "slide_from_right",
+    })}
+  >
+    <Stack.Screen
+      name="Login"
+      component={Login}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Register"
+      component={Register}
+      options={{ title: "Register", navigateTo: "Login" }}
+    />
   </Stack.Navigator>
 );
 
@@ -50,11 +123,15 @@ export default function App() {
 }
 
 function AppNavigator() {
-  const { isAuthenticated } = useContext(AuthContext); 
+  const { isAuthenticated } = useContext(AuthContext);
 
   return (
     <NavigationContainer>
-      {isAuthenticated ? <AuthenticatedNavigator /> : <UnauthenticatedNavigator />}
+      {isAuthenticated ? (
+        <AuthenticatedNavigator />
+      ) : (
+        <UnauthenticatedNavigator />
+      )}
     </NavigationContainer>
   );
 }
