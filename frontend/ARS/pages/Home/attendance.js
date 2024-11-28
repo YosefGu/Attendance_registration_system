660289ba398d2100c7dffe5f3633c8a8bad208c9
YosefGu <yos770yos@gmail.com>
2024-11-28 14:39:05 +0200

    updating attendance file

--- frontend/ARS/pages/Home/attendance.js
+++ frontend/ARS/pages/Home/attendance.js
@@ -1,88 +1,103 @@
-import { Text, View, StyleSheet, ScrollView } from "react-native"
-import { useContext, useState } from 'react'
-import styles from "../../utils/globalStyles"
-import { CustomButton } from "../../utils/customButton"
-import { CheckBox } from "react-native-elements"
-import { UserContext } from "../../context/userContext"
-import { addAttendancList } from "../../requests/attendanceRequests"
-import { getUserID } from "../../utils/storID"
-
-
+import { Text, View, StyleSheet, ScrollView, Alert } from "react-native";
+import { useContext, useState } from "react";
+import styles from "../../utils/globalStyles";
+import { CustomButton } from "../../utils/customButton";
+import { CheckBox } from "react-native-elements";
+import { UserContext } from "../../context/userContext";
+import { addAttendancList } from "../../requests/attendanceRequests";
+import { getUserID } from "../../utils/storID";
 
 export const Attendance = () => {
-  const { state, dispatch } = useContext(UserContext)
-  const students = state.students
-  const [checkedIds, setCheckedIds] = useState(state.attendance.checkedIDs || [])
-
-
+  const { state, dispatch } = useContext(UserContext);
+  const students = state.students;
+  const [checkedIds, setCheckedIds] = useState(
+    state.attendance.checkedIDs || []
+  );
   const handleCheckbox = (studentID) => {
-    if (checkedIds.some(item => item.$oid === studentID.$oid)) {
-      setCheckedIds(checkedIds.filter(_id => _id.$oid !== studentID.$oid))
+    if (checkedIds.some((item) => item.$oid === studentID.$oid)) {
+      setCheckedIds(checkedIds.filter((_id) => _id.$oid !== studentID.$oid));
     } else {
-      setCheckedIds([...checkedIds, studentID])
+      setCheckedIds([...checkedIds, studentID]);
     }
-  }
-
+  };
 
   const handleSave = async () => {
-    const id = await getUserID()
+    const id = await getUserID();
     const data = {
-      "institutionNum": id,
-      "checkedIDs": checkedIds,
-      "uncheckedIDs": students.reduce((result, student) => {
-        if (!checkedIds.includes(student._id)) result.push(student._id)
-          return result
-      }, [])
+      institutionNum: id,
+      checkedIDs: checkedIds,
+      uncheckedIDs: students.reduce((result, student) => {
+        if (!checkedIds.includes(student._id)) result.push(student._id);
+        return result;
+      }, []),
+    };
+
+    const result = await addAttendancList(dispatch, data);
+    if (result.error) {
+      Alert.alert(result.error);
+    } else {
+      Alert.alert(result);
     }
-    await addAttendancList(dispatch, data)
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-      { 
-        students ? 
-        students.map((student, index) => (        
-          <View 
-            style={localStyle.student}
-            key={index}
-          >
-            <CheckBox  
-            checked={checkedIds.some(item => item.$oid === student._id.$oid)}
-            onPress={() => handleCheckbox(student._id)}
-            />
-            <Text
-              style={checkedIds.some(item => item.$oid === student._id.$oid) ? localStyle.checkedText : localStyle.uncheckedText}
-            > {`${student.name} ${student.lName}`}
-            </Text>
-          </View>
-         
-        ))
-        : null
-      }
+      <Text style={styles.title}>רישום נוכחות</Text>
+      <ScrollView style={style.scrollView}>
+        {students &&
+          students.map((student, index) => (
+            <View style={style.personCard} key={index}>
+              <CheckBox
+                checked={checkedIds.some(
+                  (item) => item.$oid === student._id.$oid
+                )}
+                onPress={() => handleCheckbox(student._id)}
+              />
+              <Text
+                style={[
+                  checkedIds.some((item) => item.$oid === student._id.$oid)
+                    ? style.checkedText
+                    : style.uncheckedText,
+                  styles.title2,
+                ]}
+              >
+                {" "}
+                {`${student.name} ${student.lName}`}
+              </Text>
+            </View>
+          ))}
       </ScrollView>
-      <CustomButton 
-        title='שמור'
-        onPress={handleSave}
-      />
-   </View>
-  )
-}
+      <View style={style.buttonContainer}>
+        <CustomButton title="שמור" onPress={handleSave} />
+      </View>
+    </View>
+  );
+};
 
-const localStyle = StyleSheet.create({
-  student: {
-    backgroundColor:'#7771',
-    margin:2,
-    flexDirection:'row-reverse',
-    alignItems:'center'
+const style = StyleSheet.create({
+  scrollView: {
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+  personCard: {
+    backgroundColor: "#cce1e8",
+    margin: 5,
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    borderRadius: 10,
+    borderColor: "#095b80",
+    borderWidth: 2,
   },
   checkedText: {
-    textDecorationLine: 'line-through',
-    color: 'gray',
-    textAlign: 'right',
+    textDecorationLine: "line-through",
+    color: "gray",
+    textAlign: "right",
   },
   uncheckedText: {
-    color: 'black',
-    textAlign: 'right',
+    color: "black",
+    textAlign: "right",
   },
-})
+  buttonContainer: {
+    marginVertical: 10,
+  },
+});
