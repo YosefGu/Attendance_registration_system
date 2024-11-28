cce54848ca410a92d1bdfcf442dd6876dc4c05b8
YosefGu <yos770yos@gmail.com>
2024-11-28 14:27:24 +0200

    Adding case for new student

--- frontend/ARS/reducers/attendanceReducer.js
+++ frontend/ARS/reducers/attendanceReducer.js
@@ -1,16 +1,22 @@
-
 export const attendanceInitialState = {
-    attendance: {},
-}
+  attendance: { checkedIDs: [], uncheckedIDs: [] },
+};
 
-  
 export const attendanceReducer = (state, action) => {
-    switch (action.type) {
-      case 'SET_ATTENDANCE_LIST':
-        return { 
-            attendance: action.payload 
-        };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case "SET_ATTENDANCE_LIST":
+      return {
+        attendance: action.payload,
+      };
+    case "ADD_TO_UNCHECKED_IDS":
+      return {
+        ...state,
+        attendance: {
+          ...state.attendance,
+          uncheckedIDs: [...state.attendance.uncheckedIDs, action.payload],
+        },
+      };
+    default:
+      return state;
+  }
+};
