eb4db6b4c555b7f5e789ea6fa3a144d3a76c556e
YosefGu <yos770yos@gmail.com>
2024-11-28 14:31:05 +0200

    Updating the requests as need

--- frontend/ARS/requests/teamRequests.js
+++ frontend/ARS/requests/teamRequests.js
@@ -1,37 +1,39 @@
-import axios from 'axios';
-import { API_URL } from '@env'
-import { getToken } from '../utils/tokenHandling';
-import { getUserID } from '../utils/storID';
-
+import axios from "axios";
+import { API_URL } from "@env";
+import { getToken } from "../utils/tokenHandling";
+import { getUserID } from "../utils/storID";
 
 export const get_team_members = async (dispatch) => {
-    try {
-        const token = await getToken()
-        const id = await getUserID()
-        const response = await axios.get(`${API_URL}/get-team/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        })
-        const data = response.data.map(item => ({...item.details, id: item._id['$oid']}))
-        dispatch.team({type: "SET_TEAM" ,payload: data})
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const token = await getToken();
+    const id = await getUserID();
+    const response = await axios.get(`${API_URL}/get-team/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = response.data.map((item) => ({
+      ...item.details,
+      id: item._id["$oid"],
+    }));
+    dispatch.team({ type: "SET_TEAM", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const add_team_member = async (dispatch, data) => {
-    try {
-        const token = await getToken()
-        const response = await axios.post(`${API_URL}/add-team`, data, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        })
-        dispatch.team({type: "UPDATE_TEAM" ,payload: data})
-        return response.data
-    } catch (error) {
-        return error.response.data
-    }
-}
-
+  try {
+    const token = await getToken();
+    const response = await axios.post(`${API_URL}/add-team`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    data["id"] = response.data.user_id;
+    dispatch.team({ type: "UPDATE_TEAM", payload: data });
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
--- frontend/ARS/requests/userRequests1.js
+++ frontend/ARS/requests/userRequests1.js
@@ -1,57 +1,77 @@
+import { getUserID, storeUserID } from "../utils/storID";
+import { getToken, storeToken } from "../utils/tokenHandling";
+import { API_URL } from "@env";
+import axios from "axios";
 
-import { getUserID } from "../utils/storID"
-import { getToken } from "../utils/tokenHandling"
-import { API_URL } from '@env'
-import axios from 'axios';
+export const signup = async (data) => {
+  try {
+    const response = await axios.post(`${API_URL}/signup`, data);
+    await storeToken(response.data.access_token);
+    return "Registration was successfully completed";
+  } catch (error) {
+    return error.response.data;
+  }
+};
 
-export const getUserHook = (dispatch) => {
-    const getUserDetails = async () => {
-        const userID = await getUserID()
-        const token = await getToken()
-        try {
-            const response = await axios.get(`${API_URL}/user/${userID}`, { 
-                headers: {
-                Authorization: `Bearer ${token}`,
-            }})
-            dispatch.user({ type: 'SET_USER_DETAILS', payload: response.data})
-        } catch (error) {
-            console.log("Error fetching user: ",error.data)
-        }
+export const login = async (data) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, data);
+    await storeToken(response.data.access_token);
+    await storeUserID(response.data.user_id);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const getUserDetails = async (dispatch) => {
+  const userID = await getUserID();
+  const token = await getToken();
+  try {
+    const response = await axios.get(`${API_URL}/user/${userID}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch.user({ type: "SET_USER_DETAILS", payload: response.data });
+  } catch (error) {}
+};
+
+export const updateUser = async (dispatch, user) => {
+  const update = async () => {
+    const userID = await getUserID();
+    const token = await getToken();
+    try {
+      const response = await axios.put(`${API_URL}/user/${userID}`, user, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      dispatch.user({
+        type: "SET_USER_DETAILS",
+        payload: response.data.details,
+      });
+      return "User updated successfuly";
+    } catch (error) {
+      return error.response.data;
     }
-    return getUserDetails
-    
-}
-
-export const updateUser = async (dispatch, user) => {  
-    const update = async () => {
-        const userID = await getUserID()
-        const token = await getToken()
-        try {
-            const response = await axios.put(`${API_URL}/user/${userID}`, user, { 
-                headers: {
-                Authorization: `Bearer ${token}`,
-            }})
-            dispatch.user({ type: 'SET_USER_DETAILS', payload: response.data.details})
-            } catch (error) {
-                console.log("Error updating user: ", error.data)
-        }
-    }
-    return update()
-}
-
+  };
+  return update();
+};
 
 export const deleteUser = async (user_ID) => {
-    const del = async () => {
-        const token = await getToken()
-        try {
-            const response = await axios.delete(`${API_URL}/user/${user_ID}`, { 
-                headers: {
-                Authorization: `Bearer ${token}`,
-            }})
-            console.log(response.data)
-            } catch (error) {
-                console.log("Error deleting user: ", error.data)
-        }
+  const del = async () => {
+    const token = await getToken();
+    try {
+      const response = await axios.delete(`${API_URL}/user/${user_ID}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // console.log(response.data);
+    } catch (error) {
+      // console.log("Error deleting user: ", error.data);
     }
-    return del()
-}
+  };
+  return del();
+};
