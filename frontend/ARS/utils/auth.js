d30ac35ab9e450e5266e822fd366fabbaec8da18
YosefGu <yos770yos@gmail.com>
2024-12-17 10:13:31 +0200

    Adding eas.json file for deploy, and small changes.

--- frontend/ARS/utils/auth.js
+++ frontend/ARS/utils/auth.js
@@ -1,22 +1,25 @@
-import axios from 'axios'
-import { getToken } from './tokenHandling'
-import { API_URL } from '@env'
-
+import axios from "axios";
+import { getToken } from "./tokenHandling";
+import { API_URL } from "@env";
 
 export const verifyToken = async () => {
-    const token = await getToken()
-    if(token){
-        try {
-            const response = await axios.post(`${API_URL}/verify-token`, {}, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                } 
-            })
-        return response.data.isValid
-        } catch (error) {
-        console.error('Token verfication failed:', error)
-        return false
+  const token = await getToken();
+  if (token) {
+    try {
+      const response = await axios.post(
+        `${API_URL}/verify-token`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data.isValid;
+    } catch (error) {
+      console.error("Token verfication failed:", error);
+      return false;
     }
-    } 
-    return false;
-}
+  }
+  return false;
+};
