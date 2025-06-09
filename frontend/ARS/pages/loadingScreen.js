50749884391957a0ef525dbcfaac942e064bfa84
YosefGu <yos770yos@gmail.com>
2025-06-09 20:04:02 +0300

    Updating with text and new style

--- frontend/ARS/pages/loadingScreen.js
+++ frontend/ARS/pages/loadingScreen.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 
 export const LoadingScreen = () => (
-  <View style={styles.container}>
-    <ActivityIndicator size="large" color="#abd8eb" />
+  <View style={style.container}>
+    <Text style={style.text}>בבקשה המתן</Text>
+    <Text style={style.text}>בזמן שאנחנו טוענים...</Text>
+    <ActivityIndicator size="large" color="#A0EACD" />
   </View>
 );
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#095b80",
+    backgroundColor: "#C1F2DC",
   },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color:'#10563b',
+    textAlign:'center',
+    marginBottom: 20
+  }
 });
