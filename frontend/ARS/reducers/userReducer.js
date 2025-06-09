986d704e1cd24efb16102847e45ad115f1c176e9
YosefGu <yos770yos@gmail.com>
2025-06-09 22:31:56 +0300

    delete unused code

--- frontend/ARS/reducers/userReducer.js
+++ frontend/ARS/reducers/userReducer.js
@@ -1,20 +1,13 @@
-
-
 export const userInitialState = {
     userDetails: {},
   };
 
-  
 export const userReducer = (state, action) => {
   switch (action.type) {
     case 'SET_USER_DETAILS':
       return { 
         userDetails: action.payload 
       };
-    // case 'CLEAR_USER_DETAILS':
-    //   return { 
-    //     userDetails: {} 
-    //   };
     default:
       return state;
   }
